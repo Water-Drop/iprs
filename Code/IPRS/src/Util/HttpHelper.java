f1e68b7144a70a191b26c45d03732af730dd268b
Water-Drop <water_drop@foxmail.com>
2014-05-01 23:06:56 +0800

    add httphelper to send request to RMP
    add httphelper to send request to RMP (Test.java is a servlet for test
    the class

---
+++ Code/IPRS/src/Util/HttpHelper.java
@@ -0,0 +1,59 @@
+package iprs.util;
+
+import java.io.IOException;  
+import org.apache.commons.httpclient.*;  
+import org.apache.commons.httpclient.methods.*;  
+
+public class HttpHelper {
+	public String SendHttpRequest(String type, String url, String xml){
+		String ResponseBody = "";
+		if (type.equalsIgnoreCase("GET")){
+			GetMethod get = new GetMethod(url);
+			get.setRequestHeader("Content-type", "application/xml");
+			HttpClient httpclient = new HttpClient();
+			try {
+				httpclient.executeMethod(get);
+				ResponseBody = get.getResponseBodyAsString();
+				get.releaseConnection();
+				} catch (HttpException e) {
+					e.printStackTrace();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+		}
+		else if (type.equalsIgnoreCase("POST")){
+			PostMethod post = new PostMethod(url);
+			post.setRequestBody(xml);
+			post.setRequestHeader("Content-type", "application/xml");
+			HttpClient httpclient = new HttpClient();
+			try {
+				httpclient.executeMethod(post);
+				ResponseBody = post.getResponseBodyAsString();
+				post.releaseConnection();
+				} catch (HttpException e) {
+					e.printStackTrace();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+		}
+		else if (type.equalsIgnoreCase("PUT")){
+			PutMethod put = new PutMethod(url);
+			put.setRequestBody(xml);
+			put.setRequestHeader("Content-type", "application/xml");
+			HttpClient httpclient = new HttpClient();
+			try {
+				httpclient.executeMethod(put);
+				ResponseBody = put.getResponseBodyAsString();
+				put.releaseConnection();
+				} catch (HttpException e) {
+					e.printStackTrace();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+		}
+		else{
+		}
+		return ResponseBody;
+	}
+
+}
