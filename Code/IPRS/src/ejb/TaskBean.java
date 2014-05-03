b5bc20dd30e0e531a06901b92fda72e12fa07122
ldsink <sink.free@gmail.com>
2014-05-03 15:46:00 +0800

    Add task bean.

---
+++ Code/IPRS/src/ejb/TaskBean.java
@@ -0,0 +1,20 @@
+/*
+ * Author	: Zhou Cheng
+ * Project	: iprs
+ * Filename	: TaskBean.java
+ * Date		: May 3, 2014
+ */
+package ejb;
+
+import java.util.List;
+
+import model.Task;
+
+public class TaskBean {
+private String domain = "http://59.78.3.25:8080/RMP/Entity/";
+	
+	public int add(List<Task> ts)
+	{
+		return 0;
+	}
+}
