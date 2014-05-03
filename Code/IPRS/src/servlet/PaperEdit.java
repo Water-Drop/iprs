c4c782afc0a0729258e2dc89ea271763d38bed8d
ldsink <sink.free@gmail.com>
2014-05-03 17:40:54 +0800

    clean code.

--- Code/IPRS/src/servlet/PaperEdit.java
+++ Code/IPRS/src/servlet/PaperEdit.java
@@ -8,9 +8,7 @@ package servlet;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.ArrayList;
 import java.util.Date;
-import java.util.List;
 
 import javax.ejb.EJB;
 import javax.servlet.RequestDispatcher;
