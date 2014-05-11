b41e701ad6508b7382abdcef7afc220af9ec804c
Water-Drop <water_drop@foxmail.com>
2014-05-11 21:14:01 +0800

    modify comment bean to fix bugs

--- Code/IPRS/src/ejb/CommentBean.java
+++ Code/IPRS/src/ejb/CommentBean.java
@@ -7,10 +7,6 @@ import java.util.ArrayList;
 
 import javax.ejb.Stateless;
 
-
-
-
-
 import model.Comment;
 //import model.User;
 import model.Task;
@@ -20,14 +16,15 @@ import util.HttpHelper;
 @Stateless
 public class CommentBean {
 	private String domain = "http://59.78.3.25:8080/RMP/Entity/";
-	public int add(String tid, String content, int rate, int confidence){
+
+	public int add(String tid, String content, int rate, int confidence) {
 		int ret = 0;
 		XMLParser xp = new XMLParser("post");
 		xp.add("set", "this.Tid", tid);
 		xp.add("set", "this.Content", content);
 		xp.add("set", "this.Rate", String.valueOf(rate));
 		xp.add("set", "this.Confidence", String.valueOf(confidence));
-		SimpleDateFormat fmt = new SimpleDateFormat("yyyy-MM-dd"); 
+		SimpleDateFormat fmt = new SimpleDateFormat("yyyy-MM-dd");
 		Date lmTime = new Date();
 		xp.add("set", "this.LMTime", fmt.format(lmTime));
 		String xmlBody = xp.getXML();
@@ -37,13 +34,14 @@ public class CommentBean {
 		System.out.println(resultXML);
 		return ret;
 	}
-	public int modify(String uri, String content, int rate, int confidence){
+
+	public int modify(String uri, String content, int rate, int confidence) {
 		int ret = 0;
 		XMLParser xp = new XMLParser("put");
 		xp.add("set", "this.Content", content);
 		xp.add("set", "this.Rate", String.valueOf(rate));
 		xp.add("set", "this.Confidence", String.valueOf(confidence));
-		SimpleDateFormat fmt = new SimpleDateFormat("yyyy-MM-dd"); 
+		SimpleDateFormat fmt = new SimpleDateFormat("yyyy-MM-dd");
 		Date lmTime = new Date();
 		xp.add("set", "this.LMTime", fmt.format(lmTime));
 		String xmlBody = xp.getXML();
@@ -53,27 +51,43 @@ public class CommentBean {
 		System.out.println(resultXML);
 		return ret;
 	}
-	public List<Comment> getPaperComment(String pid){
+
+	public List<Comment> getPaperComment(String pid) {
 		List<Comment> coms = new ArrayList<Comment>();
 		String url = domain + "iprs/Task/?Task.Pid=" + pid;
 		String resultXML = HttpHelper.SendHttpRequest("get", url, null);
 		List<Task> tasks = Task.parseXML(resultXML);
-		for (int i = 0, j = tasks.size(); i < j; i++){
+		for (int i = 0, j = tasks.size(); i < j; i++) {
+			String tmp_url = domain + "iprs/Comment/?Comment.Tid="
+					+ tasks.get(i).getUri().replaceAll("/iprs/Task/", "");
+			String tmpXML = HttpHelper.SendHttpRequest("get", tmp_url, null);
+			List<Comment> tmp_com = Comment.parseXML(tmpXML);
+			for (int k = 0; k < tmp_com.size(); k++) {
+				coms.add(tmp_com.get(k));
+			}
 		}
-		//List<Comment> coms = Comment.parseXML(resultXML);
 		return coms;
 	}
-	public List<Comment> getUserComment(String uid){
+
+	public List<Comment> getUserComment(String uid) {
 		List<Comment> coms = new ArrayList<Comment>();
 		String url = domain + "iprs/Task/?Task.Uid=" + uid;
 		String resultXML = HttpHelper.SendHttpRequest("get", url, null);
 		List<Task> tasks = Task.parseXML(resultXML);
-		for (int i = 0, j = tasks.size(); i < j; i++){
+		for (int i = 0, j = tasks.size(); i < j; i++) {
+			String tmp_url = domain + "iprs/Comment/?Comment.Tid="
+					+ tasks.get(i).getUri().replaceAll("/iprs/Task/", "");
+			String tmpXML = HttpHelper.SendHttpRequest("get", tmp_url, null);
+			List<Comment> tmp_com = Comment.parseXML(tmpXML);
+			for (int k = 0; k < tmp_com.size(); k++) {
+				coms.add(tmp_com.get(k));
+			}
 		}
-		//List<Comment> coms = Comment.parseXML(resultXML);
+
 		return coms;
 	}
-	public List<Comment> getTaskComment(String tid){
+
+	public List<Comment> getTaskComment(String tid) {
 		String url = domain + "iprs/Comment/?Comment.Tid=" + tid;
 		String resultXML = HttpHelper.SendHttpRequest("get", url, null);
 		List<Comment> coms = Comment.parseXML(resultXML);
